1346075c871d00bec643733108dc52671b328682
MonamiYoshioka <my399@cam.ac.uk>
2022-02-05 20:33:58 +0000

    Finished TaskD

--- Task1D.py
+++ Task1D.py
@@ -1,1 +1,25 @@
-from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_with_station, stations_by_river
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+    
+     # Get set of rivers which have a monitoring station
+    river_set = rivers_with_station(stations)
+     # Print number of rivers with at least 1 station
+    print("{} stations".format(len(river_set)))
+    # Print first 10 of these rivers
+    print("First 10 stations: ", sorted(river_set)[:10])
+    
+    # Get dictionary of rivers as keys with a list of station objects
+    river_station_dic = stations_by_river(stations)
+    
+    list_rivers = ['River Aire', 'River Cam', 'River Thames']
+    for river in list_rivers:
+        list_stations = river_station_dic[river]
+        print("{} has stations: {}\n".format(river, sorted(list_stations)))
+    
+if __name__ == "__main__":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -8,5 +8,38 @@ geographical data.
 
 from .utils import sorted_by_key  # noqa
 
+
+# Task 1D
 def rivers_with_station(stations):
-    pass
+    """Find rivers which have monitoring stations
+
+    Args:
+        stations (list): list of MonitoringStation objects
+
+    Returns:
+        set: contains name of the rivers with a monitoring station
+    """
+    river_set = set()
+    for station in stations:
+        river_set.add(station.river)
+    return river_set
+    
+
+def stations_by_river(stations):
+    """Find stations which are by the same river
+
+    Args:
+        stations (list): list of MonitoringStation objects
+        
+    Returns:
+        dictionary: river name as key to a list of station names
+    """
+    dic_stations_river = {}
+    for station in stations:
+        key = station.river
+        if key in dic_stations_river:
+            dic_stations_river[key].append(station.name)
+        else:
+            dic_stations_river[key] = [station.name]
+        
+    return dic_stations_river
