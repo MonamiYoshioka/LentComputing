27b180e7cb79b9dd708676b8ec1d9a758612fae8
MonamiYoshioka <my399@cam.ac.uk>
2022-02-06 16:27:21 +0000

    Finished Task1E

--- Task1E.py
+++ Task1E.py
@@ -1,8 +1,13 @@
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
 
 def run():
-    pass
+    stations = build_station_list()
+    N = 9
+    
+    river_list = rivers_by_station_number(stations, N)
+    print(river_list)
 
 if __name__ == "__main__":
-    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
     run()
