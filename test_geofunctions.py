3e1be8db5b663406e201603557db0f8208734819
rc859 <rc859@cam.ac.uk>
2022-02-09 14:13:15 +0000

    updated test

--- test_geofunctions.py
+++ test_geofunctions.py
@@ -1,12 +1,31 @@
 from floodsystem.geo import rivers_by_station_number
+from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
 
 """Test file for functions in geo.py"""
 
+def test_stations_by_distance():
+
+    stations = build_station_list()
+    cambridge = (52.2053, 0.1218)
+    check = stations_by_distance(stations,cambridge)
+    print(check)
+    assert check [0][1] > 0
+    assert check [-1][1] < 1000 
+
+
+def test_stations_within_radius():
+
+    stations = build_station_list()
+    cambridge = (52.2053, 0.1218)
+    assert len(stations_within_radius(stations, (cambridge), 10)) == 0
+    assert len(stations_within_radius(stations, (cambridge), 10)) == 0
+
 
 def test_rivers_by_station_number():
-    """Test to see wether the rivers_by_station_number funciton is running properly
+    """Test to see whether the rivers_by_station_number funciton is running properly
     """
     
     stations = build_station_list()
@@ -19,3 +38,6 @@ def test_rivers_by_station_number():
     assert len(list1) >= 3
     assert len(list2) >= 9
     assert len(list3) >= 15
+    
+    
+test_rivers_by_station_number()
